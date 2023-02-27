30485e09df57ab2362e29b3beb7f7e03a9fdac97
sofia flores <saflores2@uc.cl>
2023-02-27 18:08:56 -0300

    setup lecture y routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,26 @@
 Rails.application.routes.draw do
+  devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :products do
+    resources :registers, only: [:create]
+    collection do
+      get :my_products
+    end
+  end
+
+  resources :registers, only: [:update] do
+    member do
+      get :response
+      get :payment
+      get :confirmation
+    end
+
+    collection do
+      get :my_registers
+    end
+  end
 end
