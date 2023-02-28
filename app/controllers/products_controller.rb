56bdcf07ed2b33cd2933c81a7553b074636fb3e6
sofia flores <saflores2@uc.cl>
2023-02-28 17:26:18 -0300

    actualizacion de archivos originales

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,7 +10,8 @@ class ProductsController < ApplicationController
 
   def create
     @product = Product.new(product_params)
-    if @product.save#@product.photo.attached? && @list.save
+    @product.user = current_user
+    if @product.save
       redirect_to product_path(@product.id)
     else
       render :new, status: :unprocessable_entity
@@ -18,29 +19,37 @@ class ProductsController < ApplicationController
   end
 
   def show
+    @product = Product.find(params[:id])
   end
 
   def edit
+    @product = Product.find(params[:id])
   end
 
   def update
+    @product = Product.find(params[:id])
     @product.update(product_params)
-    redirect_to product_path(@product)
+    redirect_to product_path(@product.id)
   end
 
   def destroy
+    @product = Product.find(params[:id])
     @product.destroy
     # No need for app/views/restaurants/destroy.html.erb
     redirect_to products_path, status: :see_other
   end
 
+  def my_products
+  end
+
   private
 
   def product_params
-    params.require(:product).permit(:name, :year, :brand, :operator, :model, :description, :status, :price, :available, photos: [])
+    params.require(:product).permit(:name, :year, :brand, :operator, :model, :description, :status, :price, :available,
+                                    photos: [])
   end
 
   def set_product
-    @products = Products.find(params[:id])
+    @product = Product.find(params[:id])
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -2,5 +2,5 @@ class Product < ApplicationRecord
   belongs_to :user
   has_many_attached :photos
   validates :name, :year, :brand, :model, :status, :price, presence: true
-  validates_inclusion_of :status, in: ["Nuevo", "Usado"]
+  # validates_inclusion_of :status, in: ["Nuevo", "Usado"]
 end
