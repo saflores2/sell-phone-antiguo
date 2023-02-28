b1e501994d22b8da9c5af29589a93111a6080738
Fabian Rubio <fabian.rubiocs@gmail.com>
2023-02-28 15:51:53 -0300

    corrigo references modelo register

--- db/migrate/20230228184355_create_registers.rb
+++ db/migrate/20230228184355_create_registers.rb
@@ -1,8 +1,8 @@
 class CreateRegisters < ActiveRecord::Migration[7.0]
   def change
     create_table :registers do |t|
-      t.references :user_id, null: false, foreign_key: true
-      t.references :product_id, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
+      t.references :product, null: false, foreign_key: true
       t.boolean :status
 
       t.timestamps
